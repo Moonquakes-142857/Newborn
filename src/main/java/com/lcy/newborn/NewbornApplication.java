fca67da2c0b077a230351aabf3b7454630ead792
一粒尘埃 <extra7777777@163.com>
2020-10-23 17:58:37 +0800

    初始

---
+++ src/main/java/com/lcy/newborn/NewbornApplication.java
@@ -0,0 +1,13 @@
+package com.lcy.newborn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewbornApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewbornApplication.class, args);
+    }
+
+}
